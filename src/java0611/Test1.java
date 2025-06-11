20f64c99b2c1515b3c6aa8fa796883874957a31b
insun12 <insun0814i@naver.com>
2025-06-12 08:57:01 +0900

    fix

--- src/java0611/Test1.java
+++ src/java0611/Test1.java
@@ -3,5 +3,7 @@ package java0611;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.print("A");
+        System.out.print("b");
     }
 }
