7cc8ac1d6a4bce5e5a8be60900aae5234d66c1c5
insun12 <insun0814i@naver.com>
2025-06-13 12:16:09 +0900

    d

--- src/a1_type/TypeChange3.java
+++ src/a1_type/TypeChange3.java
@@ -2,6 +2,10 @@ package a1_type;
 
 public class TypeChange3 {
     public static void main(String[] args) {
-        
+// 기본자료형간의 연산
+        int value1 = 3 + 5; // 8
+        int value2 = 8 / 5; // 1
+        float value3 = 3.0F +5.0F; // 8.0
+        double value4 = 8.0 / 5.0; // 1.5
     }
 }
